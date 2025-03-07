67925deb63affa96964a151ee2f86e312e3d37ef
geleco <rangel001@gmail.com>
2025-03-07 09:23:40 -0300

    feat: create navbar

--- out/production/projetos/frontend/src/App.jsx
+++ out/production/projetos/frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesComponent from './routes';
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <Router>
+            <Navbar/>
+
       <RoutesComponent />
     </Router>
   );
