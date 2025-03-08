404836e55a87f502130c07cd3e7bd2057f03ef07
Joao Aecio <joaolaytynher@gmail.com>
2025-03-08 19:52:23 -0300

    feat: configuração do app.jsx

--- out/production/projetos/frontend/src/App.jsx
+++ out/production/projetos/frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import RoutesComponent from './routes';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import RoutesComponent from "./routes";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
+      <Sidebar />
+      <Footer />
       <RoutesComponent />
     </Router>
   );
