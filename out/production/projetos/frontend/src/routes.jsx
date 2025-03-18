30063a6386ef199bb8e423d01148a180de820c06
Joao Aecio <joaolaytynher@gmail.com>
2025-03-18 20:14:18 -0300

    build: criacao das rotas

--- out/production/projetos/frontend/src/routes.jsx
+++ out/production/projetos/frontend/src/routes.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Estoque from './pages/Estoque';
-import Produtos from './pages/Produtos';
-import Relatorio from './pages/Relatorio';
-import Error from './pages/Error';
-import Login from './pages/Login';
-import EsqueciSenha from './pages/EsqueciSenha';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Estoque from "./pages/Estoque";
+import Produtos from "./pages/Produtos";
+import Relatorio from "./pages/Relatorio";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+import EsqueciSenha from "./pages/EsqueciSenha";
+import UsuarioCadastro from "./pages/user/UsuarioCadastro";
+import UsuarioIndex from "./pages/user/UsuarioIndex";
+import UsuarioEditar from "./pages/user/UsuarioEditar";
 
 const RoutesComponent = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} /> {/* Define a rota inicial como Login */}
-      <Route path="/dashboard" element={<Dashboard />} /> {/* Adiciona a rota para o Dashboard */}
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/esquecisenha" element={<EsqueciSenha />} />
       <Route path="/estoque" element={<Estoque />} />
       <Route path="/produtos" element={<Produtos />} />
       <Route path="/relatorios" element={<Relatorio />} />
+      <Route path="/usuariocadastro" element={<UsuarioCadastro />} />
+      <Route path="/usuarioindex" element={<UsuarioIndex />} />
+      <Route path="/usuarioeditar" element={<UsuarioEditar />} />
       <Route path="/error" element={<Error />} />
     </Routes>
   );
