d7f6be401ab344aab0473aef457bb31efcb776a7
Joao Aecio <joaolaytynher@gmail.com>
2025-03-19 18:54:35 -0300

    feat: href para dashboard

--- out/production/projetos/frontend/src/pages/user/UsuarioIndex.jsx
+++ out/production/projetos/frontend/src/pages/user/UsuarioIndex.jsx
@@ -20,7 +20,7 @@ const UsuarioIndex = () => {
         {/* Botão de Voltar (vermelho) */}
         <a
           className="inline-block min-w-[120px] px-6 py-3 text-center rounded-sm border border-red-600 bg-red-600 text-sm font-medium text-white hover:bg-transparent hover:text-red-600 focus:ring-3 focus:outline-hidden"
-          href="#"
+          href="/dashboard"
         >
           Voltar
         </a>
