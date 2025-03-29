81f8f1c1a9f97f210a74a02e2a86f3232e7b15ae
Joao Aecio <joaolaytynher@gmail.com>
2025-03-29 18:56:30 -0300

    feat: integração

--- out/production/projetos/frontend/src/services/api.js
+++ out/production/projetos/frontend/src/services/api.js
@@ -1,9 +1,17 @@
-import axios from 'axios';
+// src/services/api.js
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  withCredentials: true
+  baseURL: "http://localhost:8080",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  console.log("Headers enviados:", config.headers); // Debug
+  return config;
+});
 
 export default api;
